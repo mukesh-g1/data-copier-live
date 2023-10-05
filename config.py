f68670c0751b88c5a728fb1246d31abb254287a5
mukesh-g1 <mukesh19aug1998@gmail.com>
2023-10-05 10:23:32 +0530

    Change Host Name for local development

--- config.py
+++ config.py
@@ -4,17 +4,19 @@ DB_DETAILS = {
     'dev': {
         'SOURCE_DB': {
             'DB_TYPE': 'mysql',
-            'DB_HOST': 'localhost',
+            'DB_HOST': '172.28.48.1',
             'DB_NAME': 'retail_db',
-            'DB_USER': os.environ.get('SOURCE_DB_USER'),
+            # 'DB_USER': os.environ.get('SOURCE_DB_USER'),
+            'DB_USER': 'retail_user',
             'DB_PASS': os.environ.get('SOURCE_DB_PASS'),
             'DB_PORT': 8806
         },
         'TARGET_DB': {
                     'DB_TYPE': 'postgres',
-                    'DB_HOST': 'localhost',
+                    'DB_HOST': '172.28.48.1',
                     'DB_NAME': 'retail_db',
-                    'DB_USER': os.environ.get('TARGET_DB_USER'),
+                    # 'DB_USER': os.environ.get('TARGET_DB_USER'),
+                    'DB_USER': 'retail_user',
                     'DB_PASS': os.environ.get('TARGET_DB_PASS'),
                     'DB_PORT': 5449
                 }
